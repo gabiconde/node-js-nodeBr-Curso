7aeebd3cb48c13a42e816727c997c7884e57d5ca
gabiconde <gabiconde@users.noreply.github.com>
2019-02-25 22:41:26 -0300

    Criando o próprio Array.filter

--- 04-lists/for-forIn/3.filter.js
+++ 04-lists/for-forIn/3.filter.js
@@ -8,17 +8,36 @@ const { obterPessoa } = require('./service')
 // const { idade, nome } = jaja
 // console.log(nome, idade)
 
+Array.prototype.meuFilter = function(callback){
+    const lista = []
+    for(item of this){
+        const result = callback(item, this)
+        if(!result) continue;
+        lista.push(item)
+    }
+    return lista;
+}
+
 async function main(params) {
     try {
+        //traz somente o results de dentro do pessoas
         const { results } = await obterPessoa('a')
-        const familiaLars = results.filter(function(item){
-            // // por padrão precisa retornar um boolean
-            // para informar se deve retirar ou manter na lista 
-            // false> remove true> mantem 
-            // encontrou = posicaoNoArray
-            const result =item.name.toLowerCase().indexOf('lars') != -1
-            return result
+        // const familiaLars = results.filter(function(item){
+        //     // // por padrão precisa retornar um boolean
+        //     // para informar se deve retirar ou manter na lista 
+        //     // false> remove true> mantem 
+        //     // encontrou = posicaoNoArray
+        //     const result = item.name.toLowerCase().indexOf('lars') !== -1
+        //     return result
+        // })
+        const familiaLars = results.meuFilter(
+            //callback
+            (item, lista) => {
+            return item.name.toLowerCase().indexOf('lars') !== -1
         })
+
+        const names = familiaLars.map((pessoa) => pessoa.name)
+        console.log(names)
     } catch (error) {
         console.error(`Deu ruim ${error}`)
     }
