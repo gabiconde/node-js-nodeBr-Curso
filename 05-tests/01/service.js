9a80108cb10b791edfb6325229fbfdcdbbd21dc2
gabiconde <gabiconde@users.noreply.github.com>
2019-02-27 17:15:11 -0300

    Introdução a desenvolvimento de testes automatizados

---
+++ 05-tests/01/service.js
@@ -0,0 +1,18 @@
+const {get} = require('axios')
+const URL = 'https://swapi.co/api/people'
+
+async function obterPessoa(nome){
+    const url = `${URL}/?search=${nome}&format=json`
+    const response = await get(url)
+    return response.data.results.map(mapearPessoa)
+}
+
+function mapearPessoa(item){
+    return {
+        nome: item.name,
+        peso: item.height
+    }
+}
+module.exports ={
+    obterPessoa
+}
