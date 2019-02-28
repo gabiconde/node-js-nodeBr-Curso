5aa2c166de21b74ea90814532d2f4bfb3073cdb7
gabiconde <gabiconde@users.noreply.github.com>
2019-02-27 22:05:05 -0300

    Manipulando arquivos - CREATE

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -1,31 +1,51 @@
-const {readFile} = require('fs')
+const {readFile, writeFile} = require('fs')
 
 const { promisify } = require('util')
 
 //transforma em promise se estiver no padrão de callback
 const readFileAsync = promisify(readFile)
-
+const writeFileAsync = promisify(writeFile)
 //outra forma de obter dados do json é só utilizar o require('./herois.json)
 
 class Database {
+
     //atributos
     constructor(){
         this.NOME_ARQUIVO = 'herois.json'
     }
 
 
-
     //metodos
     //Passar o async para utilizar o await com promise
-
     async obterDadosArquivo(){
         const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8')
         return JSON.parse(arquivo.toString())
+    }
+
+    async escreverArquivo(dados){
+        await  writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados))
+        return true
 
     }
-    escreverArquivo(){
 
+    async cadastrar(heroi){
+        const dados = await this.obterDadosArquivo()
+        const id = heroi.id <= 2 ? heroi.id : Date.now();
+        //concatenar jsons
+        const heroiComId = {
+            id,
+            ...heroi
+        }
+
+        const dadosFinal = [
+            ...dados,
+            heroiComId
+        ]
+
+        const resultado = await this.escreverArquivo(dadosFinal);
+        return resultado;
     }
+
     async listar(id){
         const dados = await this.obterDadosArquivo()
         //filter deve receber um boolean
--- 06-cli/test.js
+++ 06-cli/test.js
@@ -12,24 +12,34 @@ const DEFAULT_ITEM_CADASTRAR = {
 }
 
 describe('Suite de manipulação de herois', () => {
+    before( async () => {
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+    })
     //it comeca com o objetivo
     it('Deve pesquisar herois usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
-
         //colocar o resultado entre colchetes traz a primeira posição do array
         //[posicao1, posicao2] = await .... / se nao tiver o 2 vem null ----> destructor
         const [resultado] = await database.listar(expected.id)
-
         //deepEqual compara tudo, espera que sejam identicos
         deepEqual(resultado, expected)
-
         // o ok valida apenas se é true ou não
         //ok(resultado, expected)
     })
 
-    // it('deve cadastrar um heroi usando arquivos', async () => {
-    //     const expected = DEFAULT_ITEM_CADASTRAR
+    it('deve cadastrar um heroi usando arquivos', async () => {
+       
+        const expected = DEFAULT_ITEM_CADASTRAR
+        // alterar o conteudo do jason 
+        // const expected = {
+        //     ...DEFAULT_ITEM_CADASTRAR,
+        //     id: 2,
+        //     nome: 'batman'
+        // }
+        console.log(expected)
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
 
-    //     ok(null, expected)
-    // })
+        deepEqual(actual, expected)
+    })
 })
